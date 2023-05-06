487d91e53d1e84e72f264f16a6296ba1e79ff04c
Dima Bart <3088742@mail.ru>
2023-05-06 22:49:05 +0300

    add login form

--- client/src/components/password-input/input.tsx
+++ client/src/components/password-input/input.tsx
@@ -1,22 +1,51 @@
-import { Form, Input } from 'antd'
+import { Form, Input } from 'antd';
 import { NamePath } from 'antd/es/form/interface';
-import React from 'react'
+import React from 'react';
 
-type Props= {
-	name: string
-	placeholder: string
-	dependencies?: NamePath[]
-}
+type Props = {
+  name: string;
+  placeholder: string;
+  dependencies?: NamePath[];
+};
 
-export const PasswordInput = ({ name, placeholder, dependencies }:Props) => {
+export const PasswordInput = ({
+  name,
+  placeholder,
+  dependencies,
+}: Props) => {
   return (
     <Form.Item
-	 name={name}
-	 dependencies={dependencies}
-	 hasFeedback
-	 rules={[{required: true}]}
-	 >
-      <Input.Password placeholder={placeholder} size='large' />
+      name={name}
+      dependencies={dependencies}
+      hasFeedback
+      rules={[
+        { required: true, message: 'Обязательное поле' },
+        ({ getFieldValue }) => ({
+          validator(_, value) {
+            if (!value) {
+              return Promise.resolve();
+            }
+
+            if (name === 'confirmPassword') {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(
+                new Error('Пароли должны совпадать'),
+              );
+            } else {
+              if (value.length < 6) {
+                return Promise.reject(
+                  new Error('Пароль должен быть длиннее 6 символов')
+                );
+					 return Promise.resolve()
+              }
+            }
+          },
+        }),
+      ]}
+    >
+      <Input.Password placeholder={placeholder} size="large" />
     </Form.Item>
   );
 };
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Layout } from '../../components/layout';
-import { Card, Form, Row } from 'antd';
+import { Card, Form, Row, Space, Typography } from 'antd';
+import { CustomInput } from '../../components/custom-input';
+import { PasswordInput } from '../../components/password-input/input';
+import { CustomButton } from '../../components/custom-button';
+import { Link } from 'react-router-dom';
+import { Paths } from '../../paths';
 
 export const Login = () => {
   return (
@@ -8,9 +13,24 @@ export const Login = () => {
       <Row align="middle" justify="center">
         <Card title="Войдите" style={{ width: '30rem' }}>
           <Form onFinish={() => null}>
-				
-
-			 </Form>
+            <CustomInput
+              type="email"
+              name="email"
+              placeholder="email"
+            />
+            <PasswordInput name="password" placeholder="Пароль" />
+            <CustomButton
+              type="primary"
+              htmlType="submit"
+            >
+					Войти
+				</CustomButton>
+          </Form>
+			 <Space direction='vertical' size='large'>
+				<Typography.Text>
+					Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link>
+				</Typography.Text>
+			 </Space>
         </Card>
       </Row>
     </Layout>
